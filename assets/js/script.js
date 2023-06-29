9f94141c22b5072166f523e8f64659e64825282b
Daniel Lima <111375723+danieldelimajj@users.noreply.github.com>
2023-06-29 15:55:26 -0300

    Fiz alguns ajustes no projeto

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,21 @@
 const mode = document.getElementById('mode_icon');
 
+
 mode.addEventListener('click', () => { 
     const form = document.getElementById('login_form');
-    if(mode.classList.contains('fa-moon')) {
-    mode.classList.remove('fa-moon');
-    mode.classList.add('fa-sun');
-    form.classList.add('dark');
+    if(mode.classList.contains('fa-moon')) { //if have moon icon
+    mode.classList.remove('fa-moon'); //remove moon icon
+    mode.classList.add('fa-sun'); // add sun icon
+    form.classList.add('dark'); // add dark effect
     return;    
 
 }
     mode.classList.remove('fa-sun');
-    mode.classList.add('fa-moon');
     form.classList.remove('dark');
-
+    mode.classList.add('fa-moon');
+    
 });
+
+
+
 
